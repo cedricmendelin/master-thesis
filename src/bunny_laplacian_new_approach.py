b5c54f95548ca6e1a8edcebaa07067191605f1d2
Cedric Mendelin <cedric.mendelin@vi-sit.com>
2022-01-25 11:05:33 +0100

    rename plotting

--- src/bunny_laplacian_new_approach.py
+++ src/bunny_laplacian_new_approach.py
@@ -15,13 +15,13 @@ original_images, uniform_3d_angles, noisy_images, noise = create_or_load_bunny_i
 classes, reflections, rotations, correlations = create_or_load_knn_rotation_invariant(name='bunny', N=N, image_res=image_res, images=original_images, K=K)
 classes_noisy, reflections_noisy, rotations_noisy, correlations_noisy = create_or_load_knn_rotation_invariant(name='bunny_noisy', N=N, image_res=image_res, images=noisy_images, K=K, snr=snr)
 
-#dist_knn, idx_best_img_knn, angle_est_knn = rotation_invariant_knn(original_images, K=K)
-#dist_knn_noisy, idx_best_img_knn_noisy, angle_est_knn_noisy = rotation_invariant_knn(noisy_images, K=K)
+dist_knn, idx_best_img_knn, angle_est_knn = rotation_invariant_knn(original_images, K=K)
+dist_knn_noisy, idx_best_img_knn_noisy, angle_est_knn_noisy = rotation_invariant_knn(noisy_images, K=K)
 
 
 #Laplacian
 #A = create_adj_mat(classes)
-A = create_adj_mat(classes, reflections)
+A = create_adj_mat_nx(classes)
 
 embedding = calc_graph_laplacian(A, numberOfEvecs=3)
 
@@ -30,7 +30,7 @@ embedding = calc_graph_laplacian(A, numberOfEvecs=3)
 plot_3dscatter(embedding[:, 0], embedding[:, 1], embedding[:, 2],  (10,10))
 
 
-A_noisy = create_adj_mat(classes_noisy, reflections_noisy)
+A_noisy = create_adj_mat_nx(classes_noisy)
 
 embedding_noisy = calc_graph_laplacian(A_noisy, numberOfEvecs=3)
 
