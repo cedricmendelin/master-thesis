e1d8ec2364776812560ede551906ccfe0650f1b3
Cedric Mendelin <cedric.mendelin@vi-sit.com>
2022-05-31 12:51:29 +0200

    adjust slurm and add fbp validation

--- src/slurm/FBP_LoDoPaB_small.sh
+++ src/slurm/FBP_LoDoPaB_small.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#SBATCH --job-name=bm3d_small #Name of your job
+#SBATCH --job-name=fbp_large #Name of your job
 #SBATCH --cpus-per-task=4           #Number of cores to reserve
 #SBATCH --mem-per-cpu=12G           #Amount of RAM/core to reserve
 #SBATCH --time=24:00:00             #Maximum allocated time
 #SBATCH --qos=1day                 #Selected queue to allocate your job
 #SBATCH --partition=pascal
 #SBATCH --gres=gpu:3
-#SBATCH --output=log/bm3d_small.%j   #Path and name to the file for the STDOUT
-#SBATCH --error=log/bm3d__small_error.%j    #Path and name to the file for the STDERR
+#SBATCH --output=log/fbp_large.%j   #Path and name to the file for the STDOUT
+#SBATCH --error=log/fbp_large_error.%j    #Path and name to the file for the STDERR
 
 ml Python/3.6.6-fosscuda-2018b
 
@@ -17,4 +17,4 @@ cd ~/master-thesis-git-nobackup
 
 wandb offline
 
-python ./src/Bm3DValidationLoDoPaB_small.py --bm3d "RECO" --snr 0
+python ./src/FBPValidationLoDoPaB_small.py --snr 0
